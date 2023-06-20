59960683a675f77a188d4a06cbd54291a4db1019
coedun <coedun1@gmail.com>
2023-06-20 02:51:21 +0100

    Adding Hide Show functionality

---
+++ basic.js
@@ -0,0 +1,12 @@
+function functionShowHide(divName) {
+    var x = document.getElementById(divName);
+    toggle_visibility(x);
+}
+
+function toggle_visibility(x){
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
